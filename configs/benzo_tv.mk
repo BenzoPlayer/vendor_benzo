780ec5eda32aa2d08d82359c251a461a13acf532
dhacker29 <dhackerdvm@gmail.com>
2015-12-31 01:45:20 -0500

    vendor: Add keylayouts for Airmouse

--- configs/benzo_tv.mk
+++ configs/benzo_tv.mk
@@ -6,6 +6,12 @@ include vendor/benzo/configs/versions.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest
 
+# Key layout files
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/usr/keylayout/AliTV_Remote_V1_Airmouse.kl:system/usr/keylayout/AliTV_Remote_V1_Airmouse.kl \
+    vendor/benzo/prebuilt/usr/idc/AliTV_Remote_V1_Airmouse.idc:system/usr/idc/AliTV_Remote_V1_Airmouse.idc \
+
+
 # Include our custom apps
 PRODUCT_PACKAGES += \
     AppDrawer \
